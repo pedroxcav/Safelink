dc20dd595caa0c010cd5d5cfa6417426ca2a3fca
Pedro Cavalcanti <pedroxcav@gmail.com>
2025-11-18 23:50:47 -0300

    Update Repository

--- backend/safelink-api/src/main/java/com/safelink/api/configuration/SecurityConfiguration.java
+++ backend/safelink-api/src/main/java/com/safelink/api/configuration/SecurityConfiguration.java
@@ -72,7 +72,7 @@ public class SecurityConfiguration {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+        configuration.setAllowedOriginPatterns(List.of("*"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
